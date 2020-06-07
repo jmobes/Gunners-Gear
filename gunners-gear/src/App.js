023a7ab51c39ef5c78d260f3c9530162483cde5d
Joseph Moberly <jmobes15@gmail.com>
2020-06-06 22:38:44 -0700

    created new react app and started working on all the components related to the header. Generated the html for the components included in the header

---
+++ gunners-gear/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Header from "./shared/components/navigation/Header";
+
+const App = props => {
+ return <Header />;
+};
+
+export default App;
---
+++ gunners-gear/src/shared/components/navigation/Cart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Cart.css";
+
+const Cart = props => {
+  return (
+    <div>
+      <i class="fas fa-shopping-cart"></i>
+    </div>
+  );
+};
+
+export default Cart;
