bf52d8868887ec8960b82041df4d9bca657f2415
Joseph Moberly <jmobes15@gmail.com>
2020-06-11 23:14:40 -0700

    Set up my page routes using react router dom library.

---
+++ gunners-gear/src/NavLinks/Accessories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Accessories.css";
+
+const Accessories = () => {
+  return <h1>Accessories Page!</h1>
+};
+
+export default Accessories;
---
+++ gunners-gear/src/NavLinks/Shorts.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Shorts.css";
+
+const Shorts = () => {
+  return <h1>Shorts Page!</h1>
+};
+
+export default Shorts;
